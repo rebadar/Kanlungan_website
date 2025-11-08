55c4a4389abe18f044fcce01c0d5c80cc7b595c9
rebadar <eyabadar@gmail.com>
2025-11-08 20:32:53 +0800

    qw

---
+++ api/config.php
@@ -0,0 +1,26 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "kanlungan_db";
+    private $username = "root"; // Change this
+    private $password = ""; // Change this
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+?>
